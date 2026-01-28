889309244bf2b84097ee5f5034055223a6b942b0
johnsonsg <johnson.sg@gmail.com>
2026-01-28 09:00:46 -0500

    Add search clear

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,20 +1,45 @@
 import React from 'react'
 
 const Search = ({ searchTerm, setSearchTerm }) => {
-  return (
-    <div className='search'>
-      <div>
-        <img src='search.svg' alt='Search' />
-        <input
-          type='text'
-          placeholder='Search through thousands of movies'
-          value={searchTerm}
-          onChange={(e) => setSearchTerm(e.target.value)}
-        />
-      </div>
-      
-    </div>
-  )
+	return (
+		<div className="search">
+			<div>
+				<img src="search.svg" alt="Search" />
+
+				<input
+					type="text"
+					placeholder="Search through thousands of movies"
+					value={searchTerm}
+					onChange={(e) => setSearchTerm(e.target.value)}
+				/>
+
+				{searchTerm && (
+					<button
+						type="button"
+						className="clear-btn"
+						aria-label="Clear search"
+						onClick={() => setSearchTerm('')}
+					>
+						<svg
+							className="w-5 h-5"
+							aria-hidden="true"
+							xmlns="http://www.w3.org/2000/svg"
+							viewBox="0 0 24 24"
+							fill="none"
+						>
+							<path
+								stroke="currentColor"
+								strokeLinecap="round"
+								strokeLinejoin="round"
+								strokeWidth="2"
+								d="M6 18 17.94 6M18 18 6.06 6"
+							/>
+						</svg>
+					</button>
+				)}
+			</div>
+		</div>
+	)
 }
 
 export default Search
